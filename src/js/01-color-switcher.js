f8ef2618b162dabc458def0d1163243516951215
AleksMyronets <mironec.kiev.1993@gmail.com>
2023-03-20 10:52:25 +0200

    Completed task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,40 @@
+const body = document.querySelector('body');
+const startBtn = document.getElementById('srartBtn');
+const stopBtn = document.getElementById('stopBtn');
+
+let intervalId;
+
+// Додамо функцію для генерування випадкового кольору
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
+// Додамо слухача подій для кнопки "Start", зробимо активацію та дективацію кнопки "Start" та використаємо setInterval для зміни кольору раз на секунду
+
+startBtn = addEventListener('click', () => {
+
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+
+    intervalId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+
+});
+
+// Додамо слухача подій для кнопки "Stop", зробимо активацію та дективацію кнопки "Stop" та зупинемо зміну кольору
+
+stopBtn = addEventListener('click', () => {
+    
+    startBtn.disabled = false;
+    stopBtn.disable = true;
+
+    clearInterval(intervalId);
+
+});
+
+
+
+
 
